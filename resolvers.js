6a975c5fb06ab3cee0f194bfdff7770a460fd710
proll4588 <prol4588@gmail.com>
2022-10-21 13:06:43 +0700

    add prisma and db migrate to prisma

--- resolvers.js
+++ resolvers.js
@@ -1,83 +1,141 @@
-import DB from "./controllers/mysql.controller.js"
+import DB from './controllers/mysql.controller.js'
+import { PrismaClient } from '@prisma/client'
 
-const qTypes = async () => {
-  const globalTypes = await DB.query("SELECT * FROM global_type_goods")
-  const localTypes = await DB.query("SELECT * FROM local_type_goods")
-  const subTypes = await DB.query("SELECT * FROM sub_type_goods")
+const prisma = new PrismaClient()
 
-  const localTypes_subTypes = localTypes.map(localtype => {
-    const ans = {
-      id: localtype.id,
-      name: localtype.name,
-      photo: localtype.photo,
-      global_type_goods_id: localtype.global_type_goods_id,
-      subGoodsTypes: []
-    }
-
-    subTypes.forEach(subtype => {
-        if (subtype.local_type_goods_id === localtype.id) ans.subGoodsTypes.push({
-          id: subtype.id,
-          name: subtype.name,
-          photo: subtype.photo,
-        })
-    })
-
-    return ans
-  })
-
-  const globalTypes_localTypes_subTypes = globalTypes.map(globaltype => {
-    const ans = {
-      id: globaltype.id,
-      name: globaltype.name,
-      photo: globaltype.photo,
-      localGoodsTypes: []
-    }
-
-    localTypes_subTypes.forEach(localtype => {
-      if (localtype.global_type_goods_id === globaltype.id) ans.localGoodsTypes.push({
-        id: localtype.id,
-        name: localtype.name,
-        photo: localtype.photo,
-        subGoodsTypes: localtype.subGoodsTypes
-      })
-    })
-
-    return ans
-  })
-
-
-  return globalTypes_localTypes_subTypes
+const goodSelect = {
+    id: true,
+    name: true,
+    description: true,
+    main_photo: true,
+    brands: true,
+    sub_type_goods: true,
+    prices: {
+        select: {
+            price: true,
+            discount: true,
+        },
+    },
 }
 
-const qGoods = async (subId) => {
-  let goods = await DB.query(`select * from goods_catalog where sub_type_goods_id like ${subId}`)
-  
-  const brandIds = goods.map(good => good.brand_id)
-  let brandsInfo = brandIds.map(async id => await DB.query(`select * from brands where id like ${id}`))
-  brandsInfo = await Promise.all(brandsInfo)
+const createFilterQ = (filters) => {
+    const { brands, price, other } = filters
 
-  goods = goods.map(good => {
-    const brand = brandsInfo.find(el => {
-      console.log(el[0].id, good.brand_id);
-      return el[0].id === good.brand_id
-    })
+    const bq =
+        brands && brands.length !== 0
+            ? brands.map((el) => ({ brands: { name: el } }))
+            : undefined
+
+    // TODO: Если у товара есть скидка то при поиске она не учитывается
+    const prices =
+        price.min || price.max
+            ? {
+                  price: {
+                      gte: filters.price.min ? filters.price.min : undefined,
+                      lte: filters.price.max ? filters.price.max : undefined,
+                  },
+              }
+            : undefined
+
     return {
-      id: good.id,
-      name: good.name,
-      description: good.description,
-      main_photo: good.main_photo,
-      brand: brand[0]
+        OR: bq,
+        prices: prices,
     }
-  })
-
-  return goods
 }
 
+const qTypes = async () =>
+    await prisma.global_type_goods.findMany({
+        include: {
+            local_type_goods: {
+                include: {
+                    sub_type_goods: true,
+                },
+            },
+        },
+    })
+
+// TODO: Засунуть все параметры в filters
+const qGoods = async (subId, search, filters) => {
+    let searchString = search
+    let where = {}
+
+    if (!search) searchString = ''
+    if (filters) where = createFilterQ(filters)
+
+    if (subId) {
+        return await prisma.goods_catalog.findMany({
+            where: {
+                sub_type_goods_id: subId,
+                ...where,
+            },
+            select: goodSelect,
+        })
+    } else {
+        return await prisma.goods_catalog.findMany({
+            where: {
+                OR: [
+                    {
+                        name: {
+                            contains: searchString,
+                        },
+                    },
+                    {
+                        brands: {
+                            name: {
+                                contains: searchString,
+                            },
+                        },
+                    },
+                    {
+                        sub_type_goods: {
+                            name: {
+                                contains: searchString,
+                            },
+                        },
+                    },
+                ],
+                // TODO: Переопределение OR в объекте where
+                ...where,
+            },
+            select: goodSelect,
+        })
+    }
+}
+
+const qBrands = async (subId) => {
+    if (subId) {
+        const ans = await prisma.goods_catalog.findMany({
+            where: {
+                sub_type_goods_id: subId,
+            },
+            select: {
+                brands: true,
+            },
+        })
+        return ans.map((el) => el.brands)
+    }
+
+    return await prisma.brands.findMany()
+}
+
+const qGood = async (id) => {
+    return await prisma.goods_catalog.findUnique({
+        where: {
+            id: id,
+        },
+        select: goodSelect,
+    })
+}
+
+// TODO: РЕализовать поиск минимальной и максимальной цены
 const resolvers = {
     Query: {
-      types: async() => await qTypes(),
-      goods: async(_, {subId}) => await qGoods(subId)
+        types: async () => await qTypes(),
+        goods: async (_, { subId, search, filters }) =>
+            await qGoods(subId, search, filters),
+        brands: async (_, { subId }) => await qBrands(subId),
+        good: async (_, { id }) => qGood(id),
     },
-};
+}
 
 export default resolvers
--- schema.js
+++ schema.js
@@ -1,45 +1,79 @@
 import { gql } from 'apollo-server'
 
 const typeDefs = gql`
-  type GlobalGoodsType {
-    id: Int
-    name: String
-    photo: String
-    localGoodsTypes: [LocalGoodsType]
-  }
+    type GlobalGoodsType {
+        id: Int
+        name: String
+        photo: String
+        local_type_goods: [LocalGoodsType]
+    }
 
-  type LocalGoodsType {
-    id: Int
-    name: String
-    photo: String
-    subGoodsTypes: [SubGoodsType]
-  }
+    type LocalGoodsType {
+        id: Int
+        name: String
+        photo: String
+        sub_type_goods: [SubGoodsType]
+    }
 
-  type SubGoodsType {
-    id: Int
-    name: String
-    photo: String
-  }
+    type SubGoodsType {
+        id: Int
+        name: String
+        photo: String
+    }
 
-  type Brand {
-    id: Int
-    name: String
-    logo: String
-  }
+    type Brand {
+        id: Int
+        name: String
+        logo: String
+    }
 
-  type Good {
-    id: Int
-    name: String
-    description: String
-    main_photo: String
-    brand: Brand
-  }
+    type Prices {
+        price: Float
+        discount: Float
+    }
 
+    type Good {
+        id: Int
+        name: String
+        description: String
+        main_photo: String
+        brands: Brand
+        sub_type_goods: SubGoodsType
+        prices: Prices
+    }
 
-  type Query {
-    types: [GlobalGoodsType]
-    goods(subId: Int): [Good]
-  }
-`;
+    #type PriceFilter {
+    #    strat: Float
+    #    end: Float
+    #}
+
+    #type BasicFilter {
+    #    brands: [Int]
+    #    prices: [PriceFilter]
+    #}
+
+    input PriceFilter {
+        min: Float
+        max: Float
+    }
+
+    input OtherFilter {
+        name: String
+        params: [String]
+    }
+
+    input Filters {
+        brands: [String]
+        price: PriceFilter
+        other: [OtherFilter]
+    }
+
+    type Query {
+        types: [GlobalGoodsType]
+        goods(subId: Int, search: String, filters: Filters): [Good]
+        brands(subId: Int): [Brand]
+        good(id: Int!): Good
+    }
+`
 
 export default typeDefs
