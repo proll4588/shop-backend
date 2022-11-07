c040cd7b4aeeda134abe1cd7b1e375c14a1d50f7
proll4588 <prol4588@gmail.com>
2022-11-07 11:43:26 +0700

    fix

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 import * as dotenv from 'dotenv'
 dotenv.config()
+
 import { ApolloServer } from 'apollo-server'
 import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core'
 import resolvers from './resolvers.js'
 import typeDefs from './schema.js'
-import { TimestampResolver, TimestampTypeDefinition } from 'graphql-scalars'
+import { TimestampTypeDefinition } from 'graphql-scalars'
 import { context } from './auth.js'
 
 const server = new ApolloServer({
