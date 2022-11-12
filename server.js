a3c0a77c756fb7388523df088effaeaafcae9b08
proll4588 <prol4588@gmail.com>
2022-11-12 12:44:13 +0700

    add graphql-upload

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 import * as dotenv from 'dotenv'
 dotenv.config()
 
-import { ApolloServer } from 'apollo-server'
+import express from 'express'
+import { ApolloServer } from 'apollo-server-express'
 import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core'
+import graphqlUploadExpress from 'graphql-upload/graphqlUploadExpress.mjs'
 import resolvers from './resolvers.js'
 import typeDefs from './schema.js'
 import { TimestampTypeDefinition } from 'graphql-scalars'
@@ -17,6 +19,16 @@ const server = new ApolloServer({
     context: context,
 })
 
-server.listen().then(({ url }) => {
-    console.log(`🚀  Server ready at ${url}`)
-})
+await server.start()
+
+const app = express()
+app.use(graphqlUploadExpress())
+app.use(express.static('public'))
+server.applyMiddleware({ app })
+await new Promise((r) => app.listen({ port: 4000 }, r))
+
+console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
+
+// server.listen().then(({ url }) => {
+//     console.log(`🚀  Server ready at ${url}`)
+// })
